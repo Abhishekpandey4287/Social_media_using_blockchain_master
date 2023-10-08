cc1ec6a1d56704ccd7fc88971bd4323a4ee9d64f
Abhishekpandey4287 <89421912+Abhishekpandey4287@users.noreply.github.com>
2023-10-08 18:45:53 +0530

    first commit

--- app/src/main/java/com/example/social_media_using_blockchain/HomeActivity.java
+++ app/src/main/java/com/example/social_media_using_blockchain/HomeActivity.java
@@ -3,12 +3,19 @@ package com.example.social_media_using_blockchain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class HomeActivity extends AppCompatActivity {
+    private ImageView sound_dis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        sound_dis = (ImageView) findViewById(R.id.imageView);
+        Glide.with(this).load(R.drawable.musicgif).into(sound_dis);
     }
 }
