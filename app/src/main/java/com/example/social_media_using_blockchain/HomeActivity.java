1f6e1869d9bfb9608be372d998f7fc947b31a0ec
shreyatambad <52215220+codershreya2704@users.noreply.github.com>
2023-10-13 20:03:09 +0530

    Update-HomeActivity.java

--- app/src/main/java/com/example/social_media_using_blockchain/HomeActivity.java
+++ app/src/main/java/com/example/social_media_using_blockchain/HomeActivity.java
@@ -1,21 +1,54 @@
 package com.example.social_media_using_blockchain;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ImageView;
-
 import com.bumptech.glide.Glide;
 
 public class HomeActivity extends AppCompatActivity {
     private ImageView sound_dis;
+    private ImageView imageView7;
+    private static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        sound_dis = (ImageView) findViewById(R.id.imageView);
-        Glide.with(this).load(R.drawable.musicgif).into(sound_dis);
+        imageView7 = findViewById(R.id.imageView7); // Initialize the imageView7
+
+        Glide.with(this).load(R.drawable.add).into(imageView7);
+
+
+        imageView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            // Now you have the selected image URI, and you can use it as needed.
+            // For example, you can display the image in imageView7.
+            Glide.with(this).load(selectedImageUri).into(imageView7);
+        }
     }
 }
